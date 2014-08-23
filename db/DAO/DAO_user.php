e384073a8083f1dee2b0c9fd03b19194c5d7be33
unknown <eforonda@gmail.com>
2014-08-23 01:23:28 -0500

    new user db functions

--- db/DAO/DAO_user.php
+++ db/DAO/DAO_user.php
@@ -1,7 +1,26 @@
 <?php
 
-function create_user($username, $email, $password) {
-  
-}
+class DAO_user{
 
+  function DAO_user_exists($username){
+    $con = connect();
+    $sql = "SELECT id FROM user WHERE username='$username'";
+    $arr_res = mysql_query($sql) or die(mysql_error());
+    $result = true;
+    if( mysql_num_rows($arr_res) == 1 ) $result = false;    
+    disconnect($con);
+    return $result;
+  }
+
+  function DAO_create_new_user($username,$email,$pwd){
+    $con = connect();
+    $sql = "INSERT INTO user (username, email, password) VALUES ('$username','$email','$pwd')";
+    $arr_res = mysql_query($sql) or die(mysql_error());
+    $result = true;
+    if( mysql_num_rows($arr_res) == 1 ) $result = false;    
+    disconnect($con);
+    return $result;
+  }
+
+}
 ?>
