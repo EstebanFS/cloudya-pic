929e23c540cb97aa07adee75a95621f7c79c45c7
EstebanFS <seforonda@gmail.com>
2014-09-05 18:17:48 +0200

    Change some methods

--- db/DAO/DAO_image.php
+++ db/DAO/DAO_image.php
@@ -124,5 +124,97 @@ class DAO_image{
     disconnect($con);
     return $result;
   }
+
+    function DAO_select_hashtag_id($hashtag){
+    $con = connect();
+    $sql = "SELECT id FROM hashtag WHERE description like '$hashtag'";
+    $arr_res = mysql_query($sql); // or die(mysql_error());
+    $result = -1;
+    if (mysql_num_rows($arr_res) == 1) {
+      $result = mysql_fetch_array($arr_res);
+      $result = $result["id"];     
+    }
+    disconnect($con);
+    return $result;
+  }
+
+
+
+  function DAO_filter_image_by_hashTag($hashtag) {
+    $hashtag_id = DAO_select_hashtag_id($hashtag);
+    $con = connect();
+    $sql = "SELECT image.id AS id, image.title AS title, image.description AS description,
+            image.resource AS resource, image.extension AS extension, 
+            user.username AS username FROM hashtag, image_hashtag, image, user_image, user
+            WHERE hashtag.id = $hashtag_id AND image_hashtag.hashtag_id = hashtag.id AND
+            image_hashtag.image_id = image.id AND user_image.image_id = image.id";
+    $arr_res = mysql_query($sql);
+    $error = mysql_error();
+    if ($error != "") $result = -1;
+    else {
+      $result = array();
+      $i = 0;
+      while ($image = mysql_fetch_array($arr_res, MYSQL_BOTH)) {
+        $result[$i]["id"]          = $image["id"];
+        $result[$i]["title"]       = $image["title"];
+        $result[$i]["description"] = $image["description"];
+        $result[$i]["resource"]    = $image["resource"];
+        $result[$i]["extension"]   = $image["extension"];
+        $result[$i]["username"]    = $image["username"];
+        $i++;
+      }
+    }
+    disconnect($con);
+    return $result;
+  }
+
+  function DAO_select_image_id_by_title($title){
+    $con = connect();
+    $sql = "SELECT id FROM image WHERE title like '$title'";
+    $arr_res = mysql_query($sql);
+    $error = mysql_error();
+    if ($error != "") $result = -1;
+    else {
+      $result = array();
+      $i = 0;
+      while ($image = mysql_fetch_array($arr_res, MYSQL_BOTH)) {
+        $result[$i]["id"]          = $image["id"];
+        $i++;
+      }
+    }
+    disconnect($con);
+    return $result;
+  }
+
+  function DAO_filter_image_by_title($title) {
+    $array_image_id = DAO_select_image_id_by_title($title);
+    $con = connect();
+    $size = count($array_image_id);
+    $result = array();
+    $i = 0;
+    for($x = 0; $x < $size; $x++){
+      $id_image_by_title = $array_image_id[$x];
+      $sql = "SELECT image.id AS id, image.title AS title, image.description AS description,
+              image.resource AS resource, image.extension AS extension, 
+              user.username AS username, user_image.user_id FROM image, user_image, user
+              WHERE image.id = $id_image_by_title AND user_image.image_id = image.id AND user.id = user_image.user_id";
+      $arr_res = mysql_query($sql);
+      $error = mysql_error();
+      if ($error != "") $result = -1;
+      else {
+        while ($image = mysql_fetch_array($arr_res, MYSQL_BOTH)) {
+          $result[$i]["id"]          = $image["id"];
+          $result[$i]["title"]       = $image["title"];
+          $result[$i]["description"] = $image["description"];
+          $result[$i]["resource"]    = $image["resource"];
+          $result[$i]["extension"]   = $image["extension"];
+          $result[$i]["username"]    = $image["username"];
+          $i++;
+        }
+      }
+    } 
+    disconnect($con);
+    return $result;
+  }
 }
 ?>
